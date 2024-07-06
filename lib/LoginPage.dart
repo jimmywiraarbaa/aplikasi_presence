8b75eb4dbd73e49848dfa9c09279bc8b3ff6c846
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-07-06 22:35:39 +0700

    install package http dan shared prederences, menambil data API Login

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:aplikasi_presence/HomePage.dart';
+import 'package:aplikasi_presence/models/LoginResponse.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as myHttp;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,6 +14,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  late Future<String> _name, _token;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _token = _prefs.then((SharedPreferences prefs) {
+      return prefs.getString("token") ?? "";
+    });
+
+    _name = _prefs.then((SharedPreferences prefs) {
+      return prefs.getString("name") ?? "";
+    });
+
+    checkToken(_token, _name);
+  }
+
+  checkToken(token, name) async {
+    String tokenStr = await token;
+    String nameStr = await name;
+    if (tokenStr != "" && nameStr != "") {
+      Future.delayed(Duration(seconds: 1), () async {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomePage()))
+            .then((value) {
+          setState(() {});
+        });
+      });
+    }
+  }
+
+  Future login(email, password) async {
+    LoginResponModel? loginResponseModel;
+    Map<String, String> body = {"email": email, "password": password};
+
+    var response = await myHttp
+        .post(Uri.parse('http://127.0.0.1:8000/api/login'), body: body);
+
+    if (response.statusCode == 401) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Email atau kata sandi anda salah!")));
+    } else {
+      loginResponseModel =
+          LoginResponModel.fromJson(json.decode(response.body));
+
+      print("Hasil :" + response.body);
+      saveUser(loginResponseModel.data.token, loginResponseModel.data.name);
+    }
+  }
+
+  Future saveUser(token, name) async {
+    try {
+      final SharedPreferences pref = await _prefs;
+      pref.setString("name", name);
+      pref.setString("token", token);
+
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => HomePage()))
+          .then((value) {
+        setState(() {});
+      });
+    } catch (err) {
+      print('ERROR' + err.toString());
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(err.toString())));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,17 +100,19 @@ class _LoginPageState extends State<LoginPage> {
               Center(child: Text("Login")),
               SizedBox(height: 20),
               Text("Email"),
-              TextField(),
+              TextField(
+                controller: emailController,
+              ),
               SizedBox(height: 20),
               Text("Kata Sandi"),
-              TextField(),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+              ),
               SizedBox(height: 20),
               ElevatedButton(
                   onPressed: () {
-                    Navigator.of(context)
-                        .push(
-                            MaterialPageRoute(builder: (context) => HomePage()))
-                        .then((value) => (value));
+                    login(emailController.text, passwordController.text);
                   },
                   child: Text("Masuk"))
             ],
