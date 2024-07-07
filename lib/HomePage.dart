7089937149d33ba5ad5f9e87d8038f9ec1ab2961
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-07-07 12:19:30 +0700

    Mengset data HomePage dari API

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:aplikasi_presence/SimpanPage.dart';
+import 'package:aplikasi_presence/models/HomeResponse.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as myHttp;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,223 +14,203 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future<String> _name, _token;
+  HomeResponModel? homeResponModel;
+  Datum? hariIni;
+  List<Datum> riwayat = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _token = _prefs.then((SharedPreferences prefs) {
+      return prefs.getString("token") ?? "";
+    });
+
+    _name = _prefs.then((SharedPreferences prefs) {
+      return prefs.getString("name") ?? "";
+    });
+  }
+
+  Future getData() async {
+    final Map<String, String> headers = {
+      'Authorization': 'Bearer ' + await _token
+    };
+
+    var response = await myHttp.get(
+        Uri.parse('http://127.0.0.1:8000/api/get-presensi'),
+        headers: headers);
+
+    homeResponModel = HomeResponModel.fromJson(json.decode(response.body));
+    riwayat.clear();
+    homeResponModel!.data.forEach((element) {
+      if (element.isHariIni) {
+        hariIni = element;
+      } else {
+        riwayat.add(element);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Jimmy Wira Arba'a",
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 400,
-              decoration: BoxDecoration(color: Colors.blue[800]),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
+      body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return SafeArea(
+                  child: Padding(
+                padding: EdgeInsets.all(16.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "05 Juli 2024",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
+                    FutureBuilder(
+                        future: _name,
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return CircularProgressIndicator();
+                          } else {
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data!,
+                                style: TextStyle(fontSize: 16),
+                              );
+                            } else {
+                              return Text("-Unknown-",
+                                  style: TextStyle(fontSize: 16));
+                            }
+                          }
+                        }),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 400,
+                      decoration: BoxDecoration(color: Colors.blue[800]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              hariIni?.tanggal ?? "-",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      hariIni?.masuk ?? "-",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 26,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Masuk",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      hariIni?.pulang ?? "-",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 26,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Pulang",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                     SizedBox(
-                      height: 30,
+                      height: 20,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "07.00",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 26,
-                              ),
+                    Text("Riwayat Absen"),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: riwayat.length,
+                        itemBuilder: (context, index) => Card(
+                          child: ListTile(
+                            leading: Text(riwayat[index].tanggal),
+                            title: Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      riwayat[index].masuk,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Masuk",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      riwayat[index].pulang,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Pulang",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
                             ),
-                            Text(
-                              "Masuk",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22,
-                              ),
-                            )
-                          ],
+                          ),
                         ),
-                        Column(
-                          children: [
-                            Text(
-                              "16.30",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 26,
-                              ),
-                            ),
-                            Text(
-                              "Pulang",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    )
+                      ),
+                    ),
                   ],
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Riwayat Absen"),
-            Card(
-              child: ListTile(
-                leading: Text("04 Juli 2024"),
-                title: Row(
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "06.48",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Masuk",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "17.00",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Pulang",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Text("04 Juli 2024"),
-                title: Row(
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "06.48",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Masuk",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "17.00",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Pulang",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Text("04 Juli 2024"),
-                title: Row(
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "06.48",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Masuk",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "17.00",
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          "Pulang",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      )),
+              ));
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
